8e7c998acc3a5dc61bc7184fb872e1da7ce4beaf
Alex Santos <85900292+devalexsantos@users.noreply.github.com>
2022-01-19 18:52:02 -0300

    Create phone.js

---
+++ phone.js
@@ -0,0 +1,27 @@
+//PHONE NUMBER MASK
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+	
+	if (isLoading || newValue === '') {
+	var phoneNumber = g_form.getControl('u_phone_number');
+		phoneNumber.placeholder = "DDD + Number";
+		return;
+   }
+
+   //Type appropriate comment here, and begin script below
+var newText = newValue
+      .replace(/\D+/g, '')
+      .replace(/(\d{2})(\d)/, '($1) $2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+      .replace(/(-\d{4})\d+?$/, '$1');
+	if (newText == newValue){
+		return;
+	} else {
+		g_form.setValue('u_phone_number', newText);
+		var test = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/.test(newText);
+		if(!test){
+			g_form.showFieldMsg('u_phone_number', 'Digite um número válido', 'error');
+		}
+	}
+   
+}
