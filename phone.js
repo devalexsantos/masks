816b9f5d7c48d4c1cc7b6a2855ddc9ac66e5aad2
Alex Santos <85900292+devalexsantos@users.noreply.github.com>
2022-01-26 14:23:02 -0300

    otimizando o código

--- phone.js
+++ phone.js
@@ -1,27 +1,28 @@
-//PHONE NUMBER MASK
 function onChange(control, oldValue, newValue, isLoading, isTemplate) {
 	
 	if (isLoading || newValue === '') {
-	var phoneNumber = g_form.getControl('u_phone_number');
-		phoneNumber.placeholder = "DDD + Number";
+		var placeholder = g_form.getControl('u_phone_number');
+		placeholder.placeholder = "DDD + Número";
 		return;
    }
 
    //Type appropriate comment here, and begin script below
-var newText = newValue
+	var newText = newValue
       .replace(/\D+/g, '')
       .replace(/(\d{2})(\d)/, '($1) $2')
       .replace(/(\d{4})(\d)/, '$1-$2')
       .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
       .replace(/(-\d{4})\d+?$/, '$1');
+	
 	if (newText == newValue){
 		return;
-	} else {
-		g_form.setValue('u_phone_number', newText);
-		var test = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/.test(newText);
-		if(!test){
-			g_form.showFieldMsg('u_phone_number', 'Digite um número válido', 'error');
-		}
+	}
+	
+	g_form.setValue('u_phone_number', newText);
+	
+	var test = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/.test(newText);
+	if(!test){
+		g_form.showFieldMsg('u_phone_number', 'Digite um número válido', 'error');
 	}
    
 }
